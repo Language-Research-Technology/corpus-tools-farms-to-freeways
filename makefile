68f77556747fff2f53b2c81609770d2fde029f93
Moises Sacal <moisbo@gmail.com>
2021-11-12 10:43:13 +1100

    cleanup and modded convention for makefile

--- makefile
+++ makefile
@@ -1,3 +1,19 @@
-repo: 
-	rm -rf ../f2f-repo
-	node convert -r ../f2f-repo/ -n ATAP -s farms-to-freeways -t ~/cloudstor/atap-repo-misc/farms_to_freeways  -d ~/cloudstor/atap-repo-misc/farms_to_freeways_csv_files
+#! /bin/bash
+# MakeFile for creating Farms To Freeways corpus repo
+BASE_DIR=~/cloudstor/Shared
+REPO_OUT_DIR=./ocfl-repo
+
+REPO_NAME=ATAP
+NAMESPACE=farms-to-freeways
+TEMP_DIR=${BASE_TEMP_DIR}/temp
+DATA_DIR=${BASE_DIR}/atap-repo-misc/farms_to_freeways
+
+.DEFAULT_GOAL := repo
+
+repo:
+	node convert -r "${REPO_OUT_DIR}/" \
+	-t {DATA_DIR} -n ${REPO_NAME} \
+	-s ${NAMESPACE} -d ${BASE_DIR}/atap-repo-misc/farms_to_freeways_csv_files
+
+clean:
+	rm -rf "${TEMP_DIR}"
--- svg2csv.js
+++ svg2csv.js
@@ -1,88 +1,85 @@
-
 const fs = require("fs");
 const path = require("path");
 
 
+async function main() {
+  for (let f of fs.readdirSync("./svgfiles")) {
+    if (f.match("\.svg")) {
+      var rows = [];
+      var currentRow = { text: "", speaker: "", notes: "", timecode: "" };
+      const text = fs.readFileSync(path.join("svgfiles", f)).toString();
+      var started = false;
+      var lastSpeaker
+      for (let line of text.split("\n")) {
+        //console.log(line)
+        var speaker
+        var timecode
 
-
-async function main(){
-    for (let f of fs.readdirSync("./svgfiles")) {
-        if (f.match("\.svg")) {
-            var rows = [];
-            var currentRow = {text: "", speaker: "", notes: "", timecode: ""};
-            const text = fs.readFileSync(path.join("svgfiles", f)).toString();
-            var started = false;
-            var lastSpeaker
-            for (let line of text.split("\n")) {
-                //console.log(line)
-                var speaker
-                var timecode
-
-                if (line.match(`font-weight="700"`)) {
-                    speaker = "A"
-                } else   {
-                    speaker = "B"
-                }
-                line = line.replace(/<.*?>/g, "");
-                line = line.replace(/\s*/, "");
-                line = line.replace(/^\d+$/);
-                line = line.replace(/&apos;/, "'");
-                line = line.replace(/&apos;/, "'");
-                line = line.replace(/"/, `""`);
-
-
-                
-
-                if (!line) {speaker = lastSpeaker}
-
-                if (line.match(/^\d+\.\d+\s+$/)) {
-                    timecode = line.replace(/\s+/, "");
-                    started = true;
-                    line = "";
-                    rows.push(currentRow);
-                    currentRow = {text: "", speaker: "", notes: "", timecode: ""};
-
-                    currentRow.timecode = timecode
-                    currentRow.speaker = speaker 
-                    //csv += `"\n"${timecode}","${speaker}","`
-                } else if (!(speaker === lastSpeaker)) {
-                    if (currentRow.text) {
-                        rows.push(currentRow);
-                        currentRow = {text: "", speaker: "", notes: "", timecode: ""};
-
-                    }
-                    if (started) {
-                        currentRow.speaker = speaker
-                        currentRow.text = line
-                        //csv += `"\n"","${speaker}","${line}`;
-                    } else {
-                        currentRow.notes = line
-                        //csv += `"\n"","","", "${line}`;
-                    }
-
-                } else if (line) {
-                    if (started) {
-                        currentRow.text += line
-                    } else {
-                        currentRow.notes += line
-                    }
-                    
-                    //csv += line
-                }
-                lastSpeaker = speaker;         
-
+        if (line.match(`font-weight="700"`)) {
+          speaker = "A"
+        } else {
+          speaker = "B"
         }
-        //csv += `"`;
-        rows.push(currentRow)
+        line = line.replace(/<.*?>/g, "");
+        line = line.replace(/\s*/, "");
+        line = line.replace(/^\d+$/);
+        line = line.replace(/&apos;/, "'");
+        line = line.replace(/&apos;/, "'");
+        line = line.replace(/"/, `""`);
+
+
+        if (!line) {
+          speaker = lastSpeaker
+        }
+
+        if (line.match(/^\d+\.\d+\s+$/)) {
+          timecode = line.replace(/\s+/, "");
+          started = true;
+          line = "";
+          rows.push(currentRow);
+          currentRow = { text: "", speaker: "", notes: "", timecode: "" };
+
+          currentRow.timecode = timecode
+          currentRow.speaker = speaker
+          //csv += `"\n"${timecode}","${speaker}","`
+        } else if (!(speaker === lastSpeaker)) {
+          if (currentRow.text) {
+            rows.push(currentRow);
+            currentRow = { text: "", speaker: "", notes: "", timecode: "" };
+
+          }
+          if (started) {
+            currentRow.speaker = speaker
+            currentRow.text = line
+            //csv += `"\n"","${speaker}","${line}`;
+          } else {
+            currentRow.notes = line
+            //csv += `"\n"","","", "${line}`;
+          }
+
+        } else if (line) {
+          if (started) {
+            currentRow.text += line
+          } else {
+            currentRow.notes += line
+          }
+
+          //csv += line
+        }
+        lastSpeaker = speaker;
+
+      }
+      //csv += `"`;
+      rows.push(currentRow)
     }
     var csv = `"time","speaker","text","notes"\n`;
     for (let row of rows) {
-        if (row.timecode || row.speaker || row.notes || row.text) {
-            csv += `"${row.timecode}","${row.speaker}","${row.text}","${row.notes}"\n`;
-        }
+      if (row.timecode || row.speaker || row.notes || row.text) {
+        csv += `"${ row.timecode }","${ row.speaker }","${ row.text }","${ row.notes }"\n`;
+      }
     }
-   fs.writeFileSync(path.join("csvfiles", f.replace(".svg",".csv")), csv)
-}
+    fs.writeFileSync(path.join("csvfiles", f.replace(".svg", ".csv")), csv)
+  }
 
 }
 
