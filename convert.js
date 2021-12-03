72562df1380c3794ef8e399595ee2b0aa0d1f98d
Peter Sefton <pt@ptsefton.com>
2021-12-03 16:57:44 +1100

    Updated to be more libary based

--- convert.js
+++ convert.js
@@ -1,215 +1,33 @@
-const { ROCrate, Provenance } = require('language-data-node-tools');
-const { program } = require('commander');
-program.version('0.0.1');
-const fs = require("fs-extra");
+const {Collector} = require("oni-ocfl");
+const {languageProfileURI} = require("language-data-node-tools");
 const _ = require("lodash");
-const oniOcfl = require("oni-ocfl");
-  const tmp = require('tmp');
-  const path = require('path');
+const path = require("path");
+const { getItem } = require("../oni-ocfl/lib/ocfl-tools");
 
 
-const prov = new Provenance();
 
-const schemaStuff = [
-  // PUT IN TRANSCRIPT ETC ETC
-  {
-    "@id": "schema:startTime",
-    "@type": "rdf:Property",
-    "rdfs:comment": "The startTime of something. For a reserved event or service (e.g. FoodEstablishmentReservation), the time that it is expected to start. For actions that span a period of time, when the action was performed. e.g. John wrote a book from *January* to December. For media, including audio and video, it's the time offset of the start of a clip within a larger file.\\n\\nNote that Event uses startDate/endDate instead of startTime/endTime, even when describing dates with times. This situation may be clarified in future revisions.",
-    "rdfs:label": "startTime",
-    "schema:domainIncludes": [
-      {
-        "@id": "schema:InteractionCounter"
-      },
-      {
-        "@id": "schema:Action"
-      },
-      {
-        "@id": "schema:Schedule"
-      },
-      {
-        "@id": "schema:FoodEstablishmentReservation"
-      },
-      {
-        "@id": "schema:MediaObject"
-      }
-    ],
-    "schema:rangeIncludes": [
-      {
-        "@id": "schema:Time"
-      },
-      {
-        "@id": "#speaker",
-        "@type": "csvw:Column",
-        "csvw:datatype": "string",
-        "description": "Which of the participants is talking in that particular utterance. ",
-        "name": "Role",
-        "sameAs": {"@id": "http://www.language-archives.org/REC/role.html#speaker"}
-
-      },
-      {
-        "@id": "schema:InteractionCounter"
-      },
-      {
-        "@id": "schema:FoodEstablishmentReservation"
-      },
-      {
-        "@id": "schema:Schedule"
-      },
-      {
-        "@id": "schema:MediaObject"
-      }
-    ],
-    "schema:rangeIncludes": [
-      {
-        "@id": "schema:DateTime"
-      },
-      {
-        "@id": "schema:Time"
-      }
-    ],
-    "schema:source": {
-      "@id": "https://github.com/schemaorg/schemaorg/issues/2493"
-    }
-  },
-  {
-    "@id": "http://www.language-archives.org/REC/role.html#speaker",
-    "@type": "rdf:Property",
-    "description": "Speakers are those whose voices predominate in a recorded or filmed resource. (This resource may be a transcription of that recording.)",
-    "example": "Participants in a recorded conversation, elicitation session, or informal narration would be termed Speakers. Audience members who do not participate beyond the occasional backchannel would be termed Responders.",
-    "name": "Speaker",
-    "rdfs:comment": "The participant was a principal speaker in a resource that consists of a recording, a film, or a transcription of a recorded resource.",
-    "rdfs:label": "speaker",
-    "rdfs:subPropertyOf": {
-      "@id": "schema:contributor"
-    }
-  },
-  {
-    "@id": "http://www.language-archives.org/REC/type-20020628.html#transcription/orthographic",
-    "@type": "rdfs:Class",
-    "description": "An orthographic transcription uses a standard or conventional orthography.",
-    "name": "Orthographic transcription",
-    "rdfs:SubclassOf": {
-      "@id": "http://www.language-archives.org/REC/type-20020628.html#transcription"
-    },
-    "rdfs:comment": "Orthographic transcriptions differ from phonemic transcriptions that use a practical orthography in that they include orthographic conventions for punctuation, capitalization, etc.",
-    "rdfs:label": "OrthographicTranscription"
-  },
-  {
-    "@id": "#speaker",
-    "@type": "csvw:Column",
-    "csvw:datatype": "string",
-    "description": "Which of the participants is talking in that particular utterance. ",
-    "name": "Role"
-  },
-  {
-    "@id": "#start_time",
-    "@type": "csvw:Column",
-    "csvw:datatype": "",
-    "description": "Start time of the utterance.",
-    "name": "start_time",
-    "sameAs": { "@id": "https://schema.org/startTime" }
-  },
-  {
-    "@id": "#stop_time",
-    "@type": "csvw:Column",
-    "csvw:datatype": "",
-    "description": "End time of the utterance.",
-    "name": "stop_time",
-    "sameAs": { "@id": "https://schema.org/endTime" }
-  },
-  {
-    "@id": "#count",
-    "@type": "csvw:Column",
-    "csvw:datatype": "",
-    "description": "Utternance number",
-    "name": "",
-  },
-  {
-    "@id": "#OrthographicTranscription",
-    "@type": "csvw:Column",
-    "csvw:datatype": "csvw:Column",
-    "description": "Utternance number",
-    "name": "OrthographicTranscription",
-    "sameAs": { "@id": "http://www.language-archives.org/REC/type-20020628.html#transcription/orthographic" }
-  }
-]
-
-
-// "","Transcript","start_time","end_time","speaker","IU"
-
-const schemaTemplate = {
-  "@id": "",
-  "@type": "csvw:Schema",
-  "columns": [
-    {
-      "@id": "#start_time"
-    },
-    {
-      "@id": "#stop_time"
-    },
-    {
-      "@id": "#speaker"
-    },
-
-    {
-      "@id": "#Transcript"
-    },
-    {
-      "@id": "#count"
-    }
-  ],
-  "name": "Schema for ..."
-}
-
-
-async function addCSV(opts, crate, corpusCrateDir) {
-  const csvDir = opts.dataDir;
-  const root = crate.getRootDataset();
-  for (let extra of schemaStuff) {
-    crate.addItem(extra);
-  }
-  const schema = JSON.parse(JSON.stringify(schemaTemplate));
-  schema["@id"] = `#$table_schema`;
-  schema["name"] = `Table schema for ${ root.name } `;
-  var existingSchema = crate.getItem(schema["@id"]);
-  if (existingSchema) {
-    for (let prop of Object.keys(schema)) {
-      existingSchema[prop] = schema[prop];
-    }
-  } else {
-    crate.addItem(schema);
-  }
-  for (let item of crate.getGraph()) {
-    if (crate.utils.asArray(item["@type"]).includes("TextDialogue")) {
+async function addCSV(object) {
+  
+  for (let item of object.crate.getGraph()) {
+    if (object.crate.utils.asArray(item["@type"]).includes("TextDialogue")) {
       for (let f of item.hasFile) {
         filePath = f["@id"];
         if (filePath.match(/\.pdf/)) {
-          csvPath = filePath.replace(/\.pdf$/, ".csv")
-          var newFile = crate.getItem(csvPath);
+          var csvPath = filePath.replace(/\.pdf$/, ".csv")
+          var newFile = object.crate.getItem(csvPath);
           if (!newFile) {
             newFile = {
               "@id": csvPath,
               "name": `${ item.name } full text transcription`,
               "@type": [ "File", "OrthographicTranscription" ]
             }
-            crate.addItem(newFile);
+            object.linkDialogueSchema(newFile);
+
+            object.crate.addItem(newFile);
             item.hasFile.push({ "@id": newFile["@id"] });
           }
+          await object.addFile(newFile, path.join(object.collector.dataDir, path.basename(csvPath)));
 
-          csv = path.join(csvDir, path.basename(csvPath));
-          console.log(`Copying ${ csv } to crate ${ path.join(corpusCrateDir, csvPath) } `);
-
-          try {
-            const newFile = path.join(corpusCrateDir, csvPath);
-            fs.ensureFileSync(newFile);
-            await fs.copyFile(csv, newFile);
-            console.log(`Copied ${ csv } to crate ${ path.join(corpusCrateDir, csvPath) } `);
-          } catch (err) {
-            console.log(err);
-          }
-
-          newFile["csvw:tableSchema"] = { "@id": schema["@id"] };
           break;
         }
       }
@@ -217,72 +35,43 @@ async function addCSV(opts, crate, corpusCrateDir) {
   }
 }
 
-async function connectRepo(repoPath) {
-  const repo = await oniOcfl.connectRepo(repoPath);
-  return repo;
-
-}
 
 async function main() {
 
-  program.option('-r, --repo-path <type>', 'Path to OCFL repository')
-    .option('-n, --repo-name <type>', 'Name of OCFL repository')
-    .option('-s, --namespace <ns>', 'namespace for ARCP IDs')
-    .option('-c, --corpus-name <ns>', 'Name of this corpus/collection (if not in template)')
-    .option('-t, --template <dirs>', 'RO-Crate directory on which to base this the RO-Crate metadata file will be used as a base and any files copied in to the new collection crate')
-    .option('-d --data-dir <dirs>', "Directory of data files")
-    .option('-p --temp-path <dirs>', 'Temporary Directory Path')
-  program.parse(process.argv);
-  const opts = program.opts();
-  const repoPath = opts.repoPath;
-  const repoName = opts.repoName;
-  const dataDir = opts.dataDir;
-  const namespace = opts.namespace;
-  const corpusName = opts.corpusName; // Not used here
-  const templateDir = opts.template;
-  const tempDirPath = opts.tempPath || 'temp';
+  const coll = new Collector(); // Get all the paths etc from commandline
+  await coll.connect();
+  // Make a base corpus using template
+  console.log("Making from template", coll.templateCrateDir)
+  const corpus = coll.newObject(coll.templateCrateDir);
+  const corpusCrate = corpus.crate;
+  corpusCrate.addProfile(languageProfileURI("Collection"));
+  // Headers are "time","speaker","text","notes"
+  corpus.addDialogueSchema({"columns": ["#speaker", "#transcript", "#start_time", "#notes"]});
+  // Local name in csv colums is different from the built in one
+  const t = corpusCrate.getItem("#transcript").name = "text";
+  corpusCrate.getItem("#speaker").name = "speaker";
+  corpusCrate.getItem("#start_time").name = "time";
+  corpusCrate.getItem("#notes").name = "notes";
 
-  const repo = await connectRepo(repoPath);
 
-  if (!fs.existsSync(tempDirPath)) {
-    fs.mkdirSync(tempDirPath);
-  }
-  console.log(`Writing temp output in: ${ tempDirPath } it may not be gracefully deleted`);
-  const tmpobj = tmp.dirSync({ tmpdir: tempDirPath });
-  const corpusCrateDir = tmpobj.name;
 
-  const inputFile = path.join(templateDir, "ro-crate-metadata.json");
-  const corpusCrate = new ROCrate(JSON.parse(fs.readFileSync(inputFile)));
-  corpusCrate.index();
-  const root = corpusCrate.getRootDataset();
-  root["@type"] = corpusCrate.utils.asArray(root["@type"]);
-  root["@type"].push("RepositoryCollection");
-
-  // Add profile stuff
-
-  // TODO - get these from a repository so they are, you know, correct!
-
-  // Add provenance info
-
-  // Index by title
-  const names = {};
-  for (let item of corpusCrate.getGraph()) {
-    if (item.name) {
-      names[item.name] = item["@id"];
-    }
-  }
-
+  const root = corpus.rootDataset;
   // Make a new collection of items based on audiofiles
   interviews = {
     "@id": '#interviews',
     "name": "Interviews",
-    "@type": "SubCorpus",
+    "@type": "RepositoryCollection",
     "description": "Interview items include audio and transcripts",
     "hasMember": []
   }
+  const names = {};
+  for (let item of corpusCrate.getGraph()){
+    if (item["@type"].includes("Person")) {
+      names[item.name[0]] = item;
+    }
+  }
   for (let item of _.clone(corpusCrate.getGraph())) {
     if (item["@type"].includes("Interview Transcript")) {
-      console.log(item.name[0])
       intervieweeID = names[item.interviewee[0]];
       if (!intervieweeID) {
         console.log("Cant find", item.interviewee)
@@ -349,13 +138,15 @@ async function main() {
   for (let item of corpusCrate.getGraph()) {
     if (item["@type"] === "File") {
       filesDir.hasPart.push({ "@id": item["@id"] })
+      corpus.addFile(item, path.join(coll.templateCrateDir, item["@id"]))
     }
     if (corpusCrate.utils.asArray(item["@type"]).includes("Person")) {
       delete item.primaryTopicOf;
     }
     if (corpusCrate.utils.asArray(item["@type"]).includes("Interview Transcript") ||
       corpusCrate.utils.asArray(item["@type"]).includes("Sound")) {
-      console.log("deleting", item)
+      //console.log("deleting", item);
+      // Deleting doesnt work which is why this is building a whole new graph
     } else {
       newGraph.push(item);
     }
@@ -370,36 +161,13 @@ async function main() {
   root.hasPart = [ { "@id": filesDir["@id"] } ];
   // Clean up crate - remove unwanted Repo Objects
 
-  // Copy files across
-  for (let item of corpusCrate.getGraph()) {
-    if (corpusCrate.utils.asArray(item["@type"]).includes("File")) {
+ 
 
-      filePath = path.join(templateDir, item["@id"]);
-      const newFile = path.join(corpusCrateDir, item["@id"]);
+  await addCSV(corpus);
 
-      console.log(`Copying ${ filePath } to crate ${ newFile } `);
-
-      try {
-        fs.ensureFileSync(newFile);
-        await fs.copyFile(filePath, newFile);
-        console.log(`Copied ${ filePath } to crate ${ newFile } `);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-  }
-
-  await addCSV(opts, corpusCrate, corpusCrateDir);
-
-  const corpusID = corpusCrate.arcpId(namespace, "root", "description");
-  root.identifier = corpusID;
-  corpusCrate.addIdentifier({ name: repoName, identifier: root.identifier });
-  corpusCrate.addProvenance(prov);
-  corpusCrate.addLgProfile("Collection");
-  const outputFile = path.join(corpusCrateDir, "ro-crate-metadata.json");
-  fs.writeFileSync(outputFile, JSON.stringify(corpusCrate.getJson(), null, 2));
-  await oniOcfl.checkin(repo, repoName, corpusCrateDir, corpusCrate, "md5", "ro-crate-metadata.json");
-  // Make a new structure
+  corpus.mintArcpId("corpus","root")
+ 
+  corpus.addToRepo();
 
 }
 
--- makefile
+++ makefile
@@ -1,19 +1,20 @@
 #! /bin/bash
 # MakeFile for creating Farms To Freeways corpus repo
-BASE_DIR=~/cloudstor/Shared
+BASE_DIR=~/cloudstor/
 REPO_OUT_DIR=./ocfl-repo
 
 REPO_NAME=ATAP
 NAMESPACE=farms-to-freeways
 TEMP_DIR=${BASE_TEMP_DIR}/temp
-DATA_DIR=${BASE_DIR}/atap-repo-misc/farms_to_freeways
+DATA_DIR=${BASE_DIR}/atap-repo-misc/farms_to_freeways_csv_files
+TEMPLATE_DIR=${BASE_DIR}/atap-repo-misc/farms_to_freeways
 
 .DEFAULT_GOAL := repo
 
 repo:
 	node convert -r "${REPO_OUT_DIR}/" \
-	-t {DATA_DIR} -n ${REPO_NAME} \
-	-s ${NAMESPACE} -d ${BASE_DIR}/atap-repo-misc/farms_to_freeways_csv_files
+	-t ${TEMPLATE_DIR}  -n ${REPO_NAME} \
+	-s ${NAMESPACE} -d ${DATA_DIR}
 
 clean:
 	rm -rf "${TEMP_DIR}"
