33a7ceb05bc45e39dc39273a9149619ef4b76f6a
Moises Sacal <moisbo@gmail.com>
2022-07-08 12:59:05 +1000

    fix missing base_data_dir

--- make_ocfl_for_local_oni.sh
+++ make_ocfl_for_local_oni.sh
@@ -2,8 +2,9 @@
 
 FARMS_TO_FREEWAYS_TEMPLATE_DIR=/Users/moises/cloudstor/Shared/atap-repo-misc/farms_to_freeways
 FARMS_TO_FREEWAYS_DATA_DIR=/Users/moises/cloudstor/Shared/atap-repo-misc/farms_to_freeways_csv_files
+FARMS_TO_FREEWAYS_BASE_DATA_DIR=/Users/moises/cloudstor/Shared/atap-repo-misc
 
-make BASE_DATA_DIR=/Users/moises/cloudstor/Shared/atap-repo-misc \
+make BASE_DATA_DIR=${FARMS_TO_FREEWAYS_BASE_DATA_DIR} \
  REPO_OUT_DIR=oni/data/ocfl \
  REPO_SCRATCH_DIR=oni/data/scratch-ocfl \
  BASE_TMP_DIR=oni/data/temp \
